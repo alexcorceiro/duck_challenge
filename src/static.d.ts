9d458d5f4364178cda97e7be401b07637e9c3149
CorceiroAlex <alexcorceiro@hotmail.com>
2025-09-28 10:45:32 +0200

    version 1.2

--- src/static.d.ts
+++ src/static.d.ts
@@ -1,3 +1,45 @@
+/// <reference types="vite/client" />
+
+// Import des SVG en React components
+declare module "*.svg?react" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+// Import d’assets bruts
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+// Import des vidéos
 declare module "*.mp4" {
-    
+  const src: string;
+  export default src;
+}
+
+declare module "*.webm" {
+  const src: string;
+  export default src;
 }
